fdd056eb3cb8b7d746ebdfa73b4c52f3403afb55
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-08-06 18:11:45 +0200

    Comment failures

--- iplotProcessing/tests/test_01_expression_parsing.py
+++ iplotProcessing/tests/test_01_expression_parsing.py
@@ -9,8 +9,9 @@ class TestExpressionParsing(unittest.TestCase):
         self.parser = exprProcessing()
 
     def test_invalid_expressions(self) -> None:
-        # self.assertRaises(self.parser.setExpr("${"), ProcParsingException)
-        # self.assertRaises(self.parser.setExpr("${time"), ProcParsingException)
+        # self.assertRaises(ProcParsingException, self.parser.setExpr, "${")
+        # self.assertRaises(ProcParsingException, self.parser.setExpr, "${time")
+        self.assertRaises(ProcParsingException, self.parser.setExpr, "}")
         self.assertRaises(ProcParsingException, self.parser.setExpr, "time}")
 
     def test_vulnerabilities(self) -> None:
