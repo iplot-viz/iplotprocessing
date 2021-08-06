761087dbe1236482919e1d29a3dbb8e0e4d1a12c
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-08-06 16:43:01 +0200

    Add environment loader

---
+++ environ.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# 3-fingered-claw 
+function yell () 
+{ 
+  echo "$0: $*" >&2
+}
+
+function die () 
+{ 
+  yell "$*"; exit 1
+}
+
+function try () 
+{ 
+  "$@" || die "cannot $*" 
+}
+
+
+# Default to foss toolchain
+if [[ "$1" == "foss" || -z $1 ]];
+then
+    toolchain=foss
+elif [[ "$1" == "intel" ]];
+then
+    toolchain=intel
+fi
+echo "Toolchain: $toolchain"
+
+# Clean slate
+try module purge
+
+# Other IDV components
+try module load iplotLogging/0.0.0-GCCcore-10.2.0
+
+# Testing/Coverage requirements
+try module load coverage/5.5-GCCcore-10.2.0
+
+case $toolchain in
+
+  "foss")
+    # Array processing
+    try module load SciPy-bundle/2020.11-foss-2020b
+    ;;
+  "intel")
+    # Array processing
+    try module load SciPy-bundle/2020.11-foss-2020b
+    ;;
+   *)
+    echo "Unknown toolchain $toolchain"
+    ;;
+esac
+
+try module list -t 2>&1
