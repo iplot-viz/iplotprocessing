3290155348318bdabc75aa523994eeb9e95652aa
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-09-02 16:45:30 +0200

    Print output, cope with composite input expressions, empty data sources and empty varnames.

--- iplotProcessing/example/__main__.py
+++ iplotProcessing/example/__main__.py
@@ -1,13 +1,14 @@
 import argparse
 import os
+import sys
 import pandas as pd
 import numpy as np
 from iplotProcessing.core import Context, Processor
 from iplotProcessing.example.emulatedDataAccess import DataAccess
 from iplotLogging import setupLogger as sl
 
-logger = sl.get_logger("iplotProcessing-Example", "DEBUG")
-defaultExampleFile = os.path.join(os.path.dirname(__file__), "example_inp.csv")
+logger = sl.get_logger("iplotProcessing-Example", "INFO")
+defaultExampleFile = os.path.join(os.path.dirname(__file__), "example_inp_1.csv")
 
 
 def main():
@@ -26,9 +27,12 @@ def main():
     # The columns named 'DS', 'Variable' must be present.
     inp_file = args.input
     contents = pd.read_csv(inp_file)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.expand_frame_repr', False)
+    print(contents, file=sys.stderr)
 
     # For each row, create a processor if stackId is non zero
-    for i in range(contents.count()['DS']):
+    for i in range(contents.index.size):
         p = Processor()
         params = {
             "pulsenb": contents["PulseNumber"][i],
@@ -37,7 +41,9 @@ def main():
             "ts_end": contents["EndTime"][i],
             "ts_format": "relative" if np.isnan(contents["StartTime"][i]) or np.isnan(contents["EndTime"][i]) else "absolute"
         }
-        p.setParams(contents["DS"][i], contents["Variable"][i], **params)
+        ds = contents["DS"][i]
+        inputExpr = contents["Variable"][i]
+        p.setParams(ds, inputExpr, **params)
 
         # In order to access and share global aliases, register it
         # with the previously created global context.
@@ -49,22 +55,24 @@ def main():
             ctx.updateAlias(p.dataSource, p.inputExpr,
                             contents["Alias"][i], **params)
 
-        # Now, populate the environment, i.e, initialize key-value pairs.
-        # The 'value' is an empty 'Signal' instance
-        ctx.refresh()
+    # Now, populate the environment, i.e, initialize key-value pairs.
+    # The 'value' is an empty 'Signal' instance
+    ctx.refresh()
 
-        # Now, emulate data access and set the fetched contents as input data for processing
-        for proc in ctx.processors.values():
-            for varName in proc.varNames:
-                dobj = da.getData(proc.dataSource, varName)
-                ctx.setInputData(dobj, proc.dataSource, varName, **proc.params)
+    # Now, emulate data access and set the fetched contents as input data for processing
+    for proc in ctx.processors.values():
+        if not isinstance(proc.dataSource, str) or not len(proc.dataSource):
+            continue
+        for varName in proc.varNames:
+            dobj = da.getData(proc.dataSource, varName)
+            ctx.setInputData(dobj, proc.dataSource, varName, **proc.params)
 
     # Now query all processors.
-    for i in range(contents.count()['DS']):
+    for i in range(contents.index.size):
 
         # Get processor by DS and Variable names
-        dataSource = contents["DS"][i]
-        inputExpr = contents["Variable"][i]
+        dataSource = contents["DS"][i] if isinstance(contents["DS"][i], str) else ""
+        inputExpr = contents["Variable"][i] if isinstance(contents["Variable"][i], str) else ""
 
         params = {
             "pulsenb": contents["PulseNumber"][i],
@@ -81,6 +89,12 @@ def main():
         ydata = contents["y"][i]
         zdata = contents["z"][i]
 
+        print(f"Row {i}: xexpr = {xdata}")
         x = proc.compute(xdata)
+        print(x)
+        print(f"Row {i}: yexpr = {ydata}")
         y = proc.compute(ydata)
+        print(y)
+        print(f"Row {i}: zexpr = {zdata}")
         z = proc.compute(zdata)
+        print(z)
