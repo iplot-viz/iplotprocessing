2f0c62882585c6589a3fe7ef853ea89b49e68649
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-09-24 09:44:28 +0200

    Fix funny naming convention. Use AssertRaises

--- iplotProcessing/tests/test_01_expression_parsing.py
+++ iplotProcessing/tests/test_01_expression_parsing.py
@@ -53,22 +53,9 @@ class TestExpressionParsing(unittest.TestCase):
         for testVal, validVal in zip(self.parser.result, validResult):
             self.assertAlmostEqual(testVal, validVal)
 
-    def test_eval_WrongComplex(self) -> None:
+    def test_eval_wrong_complex(self) -> None:
         expr = "sin(${${l}}) + cos(${m}) + ${n}"
-        subst = {"${l": np.arange(0, 4, dtype=np.float64),
-                 "m": np.arange(0, 40, 10, dtype=np.float64),
-                 "n": 10.0}
-        try:
-            self.parser.set_expression(expr)
-            self.parser.substitute_var(subst)
-            self.parser.eval_expr()
-            validResult = np.frombuffer(
-                b'\x00\x00\x00\x00\x00\x00&@\x1d\xca_\x80:\x01$@\x86@x\x90\x7f\xa2&@\xbf\x1c\x80\xed:\x97$@')
-            for testVal, validVal in zip(self.parser.result, validResult):
-                self.assertAlmostEqual(testVal, validVal)
-        ##shall trigger an exception
-        except InvalidExpression:
-            pass
+        self.assertRaises(InvalidExpression, self.parser.set_expression, expr)
 
 
 
