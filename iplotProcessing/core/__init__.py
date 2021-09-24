f4cedef898ec7e16e6f99dabe7f017db7838f52a
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-09-24 16:37:41 +0200

    Expose environment class

--- iplotProcessing/core/__init__.py
+++ iplotProcessing/core/__init__.py
@@ -1,5 +1,6 @@
 from .bobject import BufferObject
 from .context import Context, SignalDescription
+from .environment import Environment
 from .signal import Signal
 
-__all__ = ["BufferObject", "Context", "Signal", "SignalDescription"]
+__all__ = ["BufferObject", "Context", "Environment", "Signal", "SignalDescription"]
