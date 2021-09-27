a8cc44c820fe65904b820d316962c2aa73e64544
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2021-09-27 12:44:36 +0200

    Add datetime units

--- iplotProcessing/common/__init__.py
+++ iplotProcessing/common/__init__.py
@@ -1,3 +1,5 @@
-from .errors import InvalidExpression, InvalidSignalName, UnboundSignal
+from .errors import InvalidExpression, InvalidSignalName, InvalidVariable, UnboundSignal
 from .table_parser import get_value
-__all__ = ["InvalidExpression", "InvalidSignalName", "UnboundSignal", "get_value"]
+from .units import DATE_TIME, PRECISE_TIME, DATE_TIME_PRECISE, DATE, TIME
+__all__ = ["DATE_TIME", "PRECISE_TIME", "DATE_TIME_PRECISE", "DATE", "TIME",
+           "InvalidExpression", "InvalidSignalName", "InvalidVariable", "UnboundSignal", "get_value"]
