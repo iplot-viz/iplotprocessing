f5a2b6c8fc46d1aab9ac7ee74f62f83952cf6268
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2022-01-16 20:19:51 +0100

    Fix failing tests in ufunc

--- iplotProcessing/tests/test_10_signal_1d_add.py
+++ iplotProcessing/tests/test_10_signal_1d_add.py
@@ -3,6 +3,7 @@
 
 import unittest
 from iplotProcessing.common.grid_mixing import GridAlignmentMode
+from iplotProcessing.common.interpolation import InterpolationKind
 from iplotProcessing.math.pre_processing.grid_mixing import align
 import numpy as np
 from iplotProcessing.core import BufferObject, Signal
@@ -22,19 +23,19 @@ class TestSignal1DAdd(unittest.TestCase):
         return super().setUp()
 
     def test_signal_ufunc_simple(self):
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = self.s1 + self.s2 + self.s3
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x18@\x00\x00\x00\x00\x00\x00"@')
 
     def test_signal_ufunc_advanced_1(self):
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = np.sin(self.s1)
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00\xee\x0c\t\x8fT\xed\xea?F\xb4\xd1\xea\xf6\x18\xed?[\xd5\xb6m8\x10\xc2?')
 
     def test_bo_ufunc_advanced_2(self):
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = np.sin(self.s1 + self.s2 + self.s3)
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00[\xd5\xb6m8\x10\xc2?\xc0\xa2\xb0\x8a\xf1\xe1\xd1\xbf\x91/\x0c6&`\xda?')
--- iplotProcessing/tests/test_11_signal_2d_add.py
+++ iplotProcessing/tests/test_11_signal_2d_add.py
@@ -3,6 +3,7 @@
 
 import unittest
 from iplotProcessing.common.grid_mixing import GridAlignmentMode
+from iplotProcessing.common.interpolation import InterpolationKind
 from iplotProcessing.math.pre_processing.grid_mixing import align
 import numpy as np
 from iplotProcessing.core import BufferObject, Signal
@@ -23,21 +24,21 @@ class TestSignal2DAdd(unittest.TestCase):
 
     def test_signal_ufunc_simple(self):
         unittest.skip('NotImplemented')
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = self.s1 + self.s2 + self.s3
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x18@\x00\x00\x00\x00\x00\x00"@')
 
     def test_signal_ufunc_advanced_1(self):
         unittest.skip('NotImplemented')
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = np.sin(self.s1)
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00\xee\x0c\t\x8fT\xed\xea?F\xb4\xd1\xea\xf6\x18\xed?[\xd5\xb6m8\x10\xc2?')
 
     def test_bo_ufunc_advanced_2(self):
         unittest.skip('NotImplemented')
-        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION)
+        align([self.s1, self.s2, self.s3], GridAlignmentMode.UNION, kind=InterpolationKind.LINEAR)
         res = np.sin(self.s1 + self.s2 + self.s3)
         self.assertEqual(res.data.tobytes(
         ), b'\x00\x00\x00\x00\x00\x00\x00\x00[\xd5\xb6m8\x10\xc2?\xc0\xa2\xb0\x8a\xf1\xe1\xd1\xbf\x91/\x0c6&`\xda?')
