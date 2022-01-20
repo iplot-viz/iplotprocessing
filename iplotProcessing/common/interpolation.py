d31b5eca524abe19cbf3a8746b37b158fb62e320
Panchumarti Jaswant EXT <jaswant.panchumarti@iter.org>
2022-01-20 10:56:30 +0100

    Remove ,

--- iplotProcessing/common/interpolation.py
+++ iplotProcessing/common/interpolation.py
@@ -3,11 +3,11 @@
 
 class InterpolationKind:
     LINEAR = 'linear'
-    NEAREST = 'nearest',
-    NEAREST_UP = 'nearest-up',
-    ZERO = 'zero',
-    SLINEAR = 'slinear',
-    QUADRATIC = 'quadratic',
-    CUBIC = 'cubic',
-    PREVIOUS = 'previous',
-    NEXT = 'next',
+    NEAREST = 'nearest'
+    NEAREST_UP = 'nearest-up'
+    ZERO = 'zero'
+    SLINEAR = 'slinear'
+    QUADRATIC = 'quadratic'
+    CUBIC = 'cubic'
+    PREVIOUS = 'previous'
+    NEXT = 'next'
